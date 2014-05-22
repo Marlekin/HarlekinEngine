cc204025ea1cca3d8cf244aea1a1a30fbe8b1213
Marlekin <simon.kohlstrom@gmail.com>
2014-05-22 21:12:47 +0200

    Changes

---
+++ HarlekinEngine/com/root/engine/MainComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarlekinEngine.com.root.engine
+{
+
+    public class MainComponent
+    {
+
+        public MainComponent()
+        {
+            
+        }
+
+
+        public static void main(String[] args)
+        {
+            Window win = Window.Instance;
+            MainComponent mc = new MainComponent();
+        }
+
+    }
+
+}
