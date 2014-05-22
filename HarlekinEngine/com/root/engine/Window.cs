77a3e6048a6b4939c7fd3d99ca57455f18459a9c
Marlekin <simon.kohlstrom@gmail.com>
2014-05-22 21:13:56 +0200

    Changes

---
+++ HarlekinEngine/com/root/engine/Window.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL4;
+
+namespace HarlekinEngine.com.root.engine
+{
+    class Window: GameWindow
+    {
+
+        private static GameWindow instance;
+
+        private Window()
+        {
+
+        }
+
+        public static Window Instance
+        {
+           get
+            {
+
+               if(instance == null)
+               {
+                   instance = new Window();
+               }
+
+               return (Window)instance;
+
+           }
+            
+        }
+
+    }
+}
